f93f67d666644b561ae21d701b5f6147d93e2695
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2024-05-05 16:56:55 +0200

    Add files via upload

---
+++ DeliverooAgent.js/lab2-BeliefRevision/memory.js
@@ -0,0 +1,16 @@
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+
+const client = new DeliverooApi(
+    'http://localhost:8080',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5ZmQ2NDllNzZlIiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjc5OTk3Njg2fQ.6_zmgL_C_9QgoOX923ESvrv2i2_1bgL_cWjMw4M7ah4'
+)
+/**
+ * @type {Map<string,[{id,x,y,carriedBy,reward}]}
+ */
+const db = new Map()
+    
+const start = Date.now();
+
+client.socket.on( 'parcels sensing', ( parcels ) => {
+
+} )
---
+++ DeliverooAgent.js/lab3-ReactiveAgent/one_move.js
@@ -0,0 +1,37 @@
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+
+const client = new DeliverooApi(
+    'http://localhost:8080',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5ZmQ2NDllNzZlIiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjc5OTk3Njg2fQ.6_zmgL_C_9QgoOX923ESvrv2i2_1bgL_cWjMw4M7ah4'
+)
+
+function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
+    const dx = Math.abs( Math.round(x1) - Math.round(x2) )
+    const dy = Math.abs( Math.round(y1) - Math.round(y2) )
+    return dx + dy;
+}
+
+const me = {};
+
+client.onYou( ( {id, name, x, y, score} ) => {
+    me.id = id
+    me.name = name
+    me.x = x
+    me.y = y
+    me.score = score
+} )
+
+const db = new Map()
+
+client.onParcelsSensing( async ( parcels ) => {
+    
+    const pretty = Array.from(parcels)
+        .map( ( {id,x,y,carriedBy,reward} ) => {
+            return reward; //`(${x},${y},${reward})`
+        } )
+        .join( ' ' )
+    console.log( pretty )
+
+} )
+
+
