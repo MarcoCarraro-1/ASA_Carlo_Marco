9a6096c47aae4f06467e1e2f778a8464ab28e96a
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2024-05-05 16:59:43 +0200

    Part 2 of DeliverooAgent folder

---
+++ DeliverooAgent.js/config.js
@@ -0,0 +1,10 @@
+const config = {
+
+    // host: "http://localhost:8080",
+    host: "https://deliveroojs.onrender.com",
+
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUyMzcwYzQ4NmZmIiwibmFtZSI6ImdvZCIsImlhdCI6MTcxNDY2ODIzMX0.xFHNDDI8yanDQiGiBY9TfWKy3twdJZs5UWKpJnzEabE'
+
+}
+
+export default config;
